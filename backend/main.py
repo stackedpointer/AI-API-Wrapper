0978faea7fd09460b9620fc05f1619700a649e76
Chaitanya <stackedpointer@gmail.com>
2026-03-06 13:42:24 +0530

    added chat history with id

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from database import DataBase
+from typing import Optional
 from llmclient import LLMCLIENT
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -20,20 +21,55 @@ app.add_middleware(
 
 class RequestData(BaseModel):
     request: str
+    chat_id: Optional[int] = None
+class TitleRequest(BaseModel):
+    title: str
+
+def generate_title(message):
+    words = message.split()
+    title = " ".join(words[:5])
+    return title
 
 @app.get("/")
 def home():
     return {"message" : "server running"}
 
 
+@app.post("/title")
+def receive_titles(data : TitleRequest):
+    chat_id = db.get_id(data.title)
+    return {"chat_id":chat_id}
+
+
+
 @app.post("/request")
-async def recieve_messages(data : RequestData):
+async def receive_messages(data : RequestData):
+    chat_id = data.chat_id
+    user_message = data.request
+
+    if chat_id is None:
+        title = generate_title(user_message)
+        chat_id = db.get_id(title)
+    db.storeUser(chat_id, user_message)
+
+    history = db.load(chat_id)
+    messages = []
+    for role, content in history:
+        messages.append({"role" :role, "content":content})
+    messages.append({"role":"user", "content": user_message})
+
     print("end point hit")
-    print(f"user sent {data.request}")
-    result = llm.generate(data.request)
+    print(data.model_dump())
+    print(f"user sent {user_message}")
+    result = llm.generate(messages)
+
+    db.storeAI(chat_id, result)
+    print("AI RESULT:", result)
 
     return{
-        "user" : data.request, "assistant" : result
+        "chat_id":chat_id,
+        "user" : data.request,
+        "assistant" : result
     }
 
 
--- frontend/script.js
+++ frontend/script.js
@@ -1,16 +1,26 @@
 console.log("JS LOADED");
 
+
+let chatId = null;
 async function sendData() {
+    console.log("BUTTOON CLICKED")
     const input = document.getElementById("userinput").value;
 
-    const response = await fetch("http://127.0.0.1:8000/request", {
+    try{const response = await fetch("http://127.0.0.1:8000/request", {
         method: "POST", 
         headers: {
         "Content-Type":"application/json"
         },
-        body: JSON.stringify({request : input})
+        body: JSON.stringify({request : input,chat_id:chatId
+        })
     });
     const data = await response.json();
-    document.getElementById("usermessage").innerText = data.user;
-    document.getElementById("aimessage").innerText = data.assistant;
-}
+    console.log(data)
+
+    chatId = data.chat_id;
+
+    document.getElementById("usermessage").innerHTML += "<p>" + data.user + "</p>";;
+    document.getElementById("aimessage").innerHTML += "<p>" + data.assistant + "</p>";
+}catch(error){
+    console.error(error)
+}}
