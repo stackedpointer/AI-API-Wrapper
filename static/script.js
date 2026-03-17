d055f062a4c7bc4f7bbeb5eb0670fcce208cb57f
Chaitanya <stackedpointer@gmail.com>
2026-03-17 17:17:25 +0530

    added new chat button

--- static/script.js
+++ static/script.js
@@ -27,6 +27,12 @@ async function loadSidebar(){
     });
 }
 
+function newChat(){
+    chatId = null;
+    document.getElementById("chat-container").innerHTML = "";
+    loadSidebar()
+}
+
 async function deleteChat(id){
     await fetch(`http://127.0.0.1:8000/chats/${id}`,{
         method: "DELETE"
@@ -61,30 +67,17 @@ async function sendData() {
 
     document.getElementById("chat-container").innerHTML += '<p class = "user-message" >' + input + '</p>';
 
-
     const chatContainer = document.getElementById("chat-container");
 
-    if (!chatContainer) {
-        console.error("chat-container element not found!");
-        return;
-    }
-
-    try{const response = await fetch("http://127.0.0.1:8000/request", {
-        method: "POST", 
-        headers: {
-        "Content-Type":"application/json"
-        },
-        body: JSON.stringify({request : input,chat_id:chatId
-        })
-    });
+    const response = await fetch("http://127.0.0.1:8000/request", {
+    method: "POST", 
+    headers: {"Content-Type":"application/json"},
+    body: JSON.stringify({request : input,chat_id:chatId})});
     const data = await response.json();
-    console.log(data)
 
     chatId = data.chat_id;
 
     document.getElementById("chat-container").innerHTML += '<p class = "ai-message" >' + data.assistant + '</p>';
 
     await loadSidebar();
-}catch(error){
-    console.error(error)
-}}
+}
